a19a21b08c02ec6ba619a148c4d08a308558c644
Xaynder Meyer <xaynder.meyer@nwktc.edu>
2024-09-11 12:19:52 -0600

    first code commit

--- App.js
+++ App.js
@@ -2,6 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  function Roll () {
+    return Math.floor(Math.random() * 6) + 1;
+  }
+  console.log(Roll);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
